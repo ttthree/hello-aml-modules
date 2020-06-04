7f72d75a0e2fef68c5eda85a55ff6063800f55d2
Jie Tong <jietong@microsoft.com>
2020-06-04 14:21:29 +0000

    update calculator pipeline

--- calculator/add.py
+++ calculator/add.py
@@ -4,6 +4,7 @@ from tempfile import mkdtemp
 
 from azureml.pipeline.wrapper.dsl.module import ModuleExecutor
 from azureml.pipeline.wrapper import Pipeline, Module, dsl
+from azureml.core.run import Run
 
 @dsl.module(name='add', description='Add two numbers - an illustrative module', job_type='basic')
 def add(left: Path, right: Path) -> Path:
@@ -11,9 +12,15 @@ def add(left: Path, right: Path) -> Path:
     r = float(Path(right.resolve() / f'data').read_text().strip())
     print('left = ', l)
     print('right = ', r)
+
+    result = l + r
+    run = Run.get_context()
+    run.log('result', result)
+    run.flush()
+
     output_path = Path(mkdtemp())
     with open(output_path / f'data', 'w') as fout:
-        fout.write(str(l + r))
+        fout.write(str(result))
     return output_path
 
 if __name__ == "__main__":
